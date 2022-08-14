51a6bfccab5ac29e8d4c2633e009c875278f4535
unknown <andrewsboateng@gmail.com>
2022-08-14 21:42:43 +0000

    Added Facility to Admin Dashboard

--- formulary/admin.py
+++ formulary/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("category_name",)}
+    list_display = (
+        "category_name",
+        "slug",
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Manufacturer)
+admin.site.register(Brand)
+admin.site.register(Generic_Attr)
+admin.site.register(Form)
+admin.site.register(Tier)
+admin.site.register(Product_List)

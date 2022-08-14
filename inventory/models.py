c507daa405e90af9f829aab9b776e8d50680449e
unknown <andrewsboateng@gmail.com>
2022-08-14 18:44:31 +0000

    Editing Site Id with Facility Name

--- inventory/models.py
+++ inventory/models.py
@@ -1,4 +1,6 @@
 from tabnanny import verbose
+
+from accounts.models import Facility
 from django.db import models
 from formulary.models import Product_List
 
@@ -13,12 +15,18 @@ class Inventory_Data(models.Model):
     stock_type = models.CharField(max_length=100, choices=stock_type_choices)
 
 
+class Site(models.Model):
+    site_id = models.CharField(max_length=100)
+    site_name = models.ForeignKey(Facility, on_delete=models.CASCADE)
+
+
 class Inventory(models.Model):
     product = models.ForeignKey(Product_List, on_delete=models.CASCADE)
     inventory_type = models.ForeignKey(
         Inventory_Data, on_delete=models.CASCADE
     )
     pack_quantity = models.IntegerField(verbose_name="Number of packs")
+    site_id = models.ForeignKey(Site, null=True, on_delete=models.SET_NULL)
 
     @property
     def unit_quantity(self):

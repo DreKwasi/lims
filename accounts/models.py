a931100cd747f3b88cefd0a3cc073806b81e76a8
unknown <andrewsboateng@gmail.com>
2022-08-14 18:55:11 +0000

    Completed Facility model

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -89,3 +90,10 @@ class Account(AbstractBaseUser):
 class Facility(models.Model):
     facility_name = models.CharField(max_length=100, unique=True, null=True)
     facility_location = models.CharField(max_length=100, null=True, blank=True)
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?\d{9,15}$",
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True
+    )  # Validators should be a list

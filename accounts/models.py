88cabf6e944e391fe67c871ec0048010f0b674b1
unknown <andrewsboateng@gmail.com>
2022-08-17 05:14:17 +0000

    Created Supplier Table

--- accounts/models.py
+++ accounts/models.py
@@ -110,3 +110,32 @@ class Facility(models.Model):
 
     def __str__(self):
         return self.facility_name
+
+
+class Supplier(models.Model):
+    supplier_name = models.CharField(max_length=100, unique=True, null=True)
+    supplier_location = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=225, unique=True)
+    contact_person = models.CharField(max_length=100, blank=True)
+    email_address = models.EmailField(max_length=100, null=True, blank=True)
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?\d{9,15}$",
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True
+    )  # Validators should be a list
+    description = models.TextField(max_length=225, blank=True, null=True)
+    supplied_products = models.ManyToManyField("formulary.Product_List")
+    created_by = models.ForeignKey(
+        Account, null=True, on_delete=models.SET_NULL
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Supplier"
+        ordering = ["-created_date"]
+
+    def __str__(self):
+        return self.supplier_name

ef5fac4176fb406e41dd12e6f9bfe581687d4dad
unknown <andrewsboateng@gmail.com>
2022-08-24 23:14:30 +0000

    Built Admin

--- formulary/admin.py
+++ formulary/admin.py
@@ -8,13 +8,59 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         "category_name",
         "slug",
+        "is_active",
+        "created_date",
     )
 
 
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Manufacturer)
-admin.site.register(Brand)
-admin.site.register(Generic_Attr)
-admin.site.register(Form)
-admin.site.register(Tier)
-admin.site.register(Product_List)
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("manufacturer_name", "is_active", "created_date")
+
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("brand_name", "is_active", "created_date")
+
+
+admin.site.register(Brand, BrandAdmin)
+
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("form", "is_active", "created_date")
+
+
+admin.site.register(Form, FormAdmin)
+
+
+class GenericAttrAdmin(admin.ModelAdmin):
+    list_display = ("generic_name", "is_active", "created_date")
+
+
+admin.site.register(Generic_Attr, GenericAttrAdmin)
+
+
+class TierAdmin(admin.ModelAdmin):
+    list_display = ("tier", "price_range", "is_active", "created_date")
+
+
+admin.site.register(Tier, TierAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "product_name",
+        "product_category",
+        "product_brand",
+        "product_form",
+        "unit_of_measure",
+        "is_active",
+        "created_date",
+    )
+
+
+admin.site.register(Product_List, ProductAdmin)

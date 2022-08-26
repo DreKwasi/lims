4fd89e3a00b26c74479478bcf36dd4f054cdc7e6
unknown <andrewsboateng@gmail.com>
2022-08-26 01:37:33 +0000

    Removed brand and tier

--- formulary/admin.py
+++ formulary/admin.py
@@ -23,13 +23,6 @@ class ManufacturerAdmin(admin.ModelAdmin):
 admin.site.register(Manufacturer, ManufacturerAdmin)
 
 
-class BrandAdmin(admin.ModelAdmin):
-    list_display = ("brand_name", "is_active", "created_date")
-
-
-admin.site.register(Brand, BrandAdmin)
-
-
 class FormAdmin(admin.ModelAdmin):
     list_display = ("form", "is_active", "created_date")
 
@@ -44,18 +37,10 @@ class GenericAttrAdmin(admin.ModelAdmin):
 admin.site.register(Generic_Attr, GenericAttrAdmin)
 
 
-class TierAdmin(admin.ModelAdmin):
-    list_display = ("tier", "price_range", "is_active", "created_date")
-
-
-admin.site.register(Tier, TierAdmin)
-
-
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         "product_name",
-        "product_category",
-        "product_brand",
+        "category",
         "product_form",
         "unit_of_measure",
         "is_active",

a0da5834f12302b5cd706b7477c83ada932f63fa
Andrews Asamoah Boateng <andrewsboateng137@gmail.com>
2022-09-17 22:35:07 +0000

    Serializers for Products List

--- formulary/api/serializers.py
+++ formulary/api/serializers.py
@@ -5,7 +5,8 @@ from rest_framework import serializers
 
 class ProductListSerializer(serializers.ModelSerializer):
     created_by = serializers.SlugRelatedField(
-        slug_field="username", read_only=True
+        slug_field="username",
+        read_only=True,
     )
     category = serializers.SlugRelatedField(
         slug_field="category_name",
@@ -13,12 +14,35 @@ class ProductListSerializer(serializers.ModelSerializer):
         read_only=False,
     )
     manufacturer = serializers.SlugRelatedField(
-        slug_field="manufacturer_name", read_only=True
+        slug_field="manufacturer_name",
+        queryset=Manufacturer.objects.all(),
+        read_only=False,
     )
     product_form = serializers.SlugRelatedField(
-        slug_field="form", read_only=True
+        slug_field="form", queryset=Form.objects.all(), read_only=False
     )
 
     class Meta:
         model = ProductList
         fields = "__all__"
+
+    def create(self, validated_data):
+        request = self.context["request"]
+        return ProductList.objects.create(
+            created_by=request.user, **validated_data
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manufacturer
+
+
+class ProductFormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Form

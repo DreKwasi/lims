41173b0798255cb84575eba36b2c27c3b8853e0f
Andrews Asamoah Boateng <andrewsboateng137@gmail.com>
2022-09-17 22:38:19 +0000

    Viewsets and custom views

--- formulary/api/views.py
+++ formulary/api/views.py
@@ -1,12 +1,25 @@
+from accounts.models import Account
 from formulary.models import ProductList
-from rest_framework import generics, mixins, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, mixins, status, viewsets
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 from rest_framework.response import Response
 
 from .serializers import ProductListSerializer
 
 
-class ProductListApiView(generics.ListCreateAPIView):
-    serializer_class = ProductListSerializer
+class ProductListApiViewset(viewsets.ModelViewSet):
     queryset = ProductList.objects.all()
-    # permission_classes = [IsAuthenticated]
+    serializer_class = ProductListSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)

506cc7c0fc89f3c4a1aa8366b596a9328bdb4a86
DreKwasi <andrewsboateng137@gmail.com>
2022-09-21 14:56:30 +0000

    Model manager for PurchaseOrderProduct

--- inbound/model_managers.py
+++ inbound/model_managers.py
@@ -1,5 +1,16 @@
 from django.db import models
+
 from .queryset import CustomQuerySet
+
+
+class PurchaseOrderManager(models.Manager):
+    def get_queryset(self):
+        return CustomQuerySet(self.model, using=self._db)
+
+
+class PurchaseOrderProductManager(models.Model):
+    def get_queryset(self):
+        return CustomQuerySet(self.model, using=self._db)
 
 
 class UnloadManager(models.Manager):

81a567982e503e93d942e6c503031c4aea3a33b4
DreKwasi <andrewsboateng137@gmail.com>
2022-11-16 06:13:32 +0000

    Serializers for All Formulary Models

--- formulary/api/serializers.py
+++ formulary/api/serializers.py
@@ -3,6 +3,50 @@ from formulary.models import *
 from rest_framework import serializers
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    category_products = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name="products-detail",
+    )
+    category_name = serializers.CharField(required=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ManufacturerSerializer(serializers.ModelSerializer):
+    manufacturer_products = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name="products-detail",
+    )
+
+    manufacturer_name = serializers.CharField(required=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        model = Manufacturer
+        fields = "__all__"
+
+
+class ProductFormSerializer(serializers.ModelSerializer):
+    form_products = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name="products-detail",
+    )
+
+    form = serializers.CharField(required=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        model = Form
+        fields = "__all__"
+
+
 class ProductListSerializer(serializers.ModelSerializer):
     product_id = serializers.ReadOnlyField()
     created_by = serializers.SlugRelatedField(
@@ -10,10 +54,9 @@ class ProductListSerializer(serializers.ModelSerializer):
         read_only=True,
     )
     category = serializers.SlugRelatedField(
-        slug_field="category_name",
-        queryset=Category.objects.all(),
-        read_only=False,
+        queryset=Category.objects.all(), slug_field="category_name"
     )
+
     manufacturer = serializers.SlugRelatedField(
         slug_field="manufacturer_name",
         queryset=Manufacturer.objects.all(),
@@ -32,39 +75,3 @@ class ProductListSerializer(serializers.ModelSerializer):
         return ProductList.objects.create(
             created_by=request.user, **validated_data
         )
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    category_products = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name="products-detail",
-    )
-
-    class Meta:
-        model = Category
-        fields = "__all__"
-
-
-class ManufacturerSerializer(serializers.ModelSerializer):
-    manufacturer_products = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name="products-detail",
-    )
-
-    class Meta:
-        model = Manufacturer
-        fields = "__all__"
-
-
-class ProductFormSerializer(serializers.ModelSerializer):
-    form_products = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name="products-detail",
-    )
-
-    class Meta:
-        model = Form
-        fields = "__all__"

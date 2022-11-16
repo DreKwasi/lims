980f00c7a54c343cae762331afd55044716f856b
DreKwasi <andrewsboateng137@gmail.com>
2022-11-16 05:59:05 +0000

    Serializers for UserAccount, Facility, Supplier

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from accounts.models import Account
+from accounts.models import Account, Facility, Supplier
 
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
+        fields = "__all__"
+
+
+class FacilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Facility
+        fields = "__all__"
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
         fields = "__all__"

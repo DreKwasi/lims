715b4839ff7d0da5da83c2be98af2bb5bd39c9d1
DreKwasi <andrewsboateng137@gmail.com>
2022-11-18 06:06:41 +0000

    Signal for Auto creating Unload when Purchase Order is saved

--- inbound/signals.py
+++ inbound/signals.py
@@ -1,120 +1,57 @@
-import random
-
-from django.contrib.contenttypes.models import ContentType
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
-from inventory.models import Inventory
+from django.db.models.signals import post_save
 
 from .models import *
 
 
-def check_area(product):
-    stock = Inventory.objects.filter(product=product)
-    if stock:
-        return stock[0].logistic_area
-    else:
-        return random.choice(LogisticArea.objects.all())
-
-
-def update_open_quantity(sender, instance, **kwargs):
-    if instance.id:
-        instance.open_quantity = (
-            instance.planned_quantity - instance.delivered_quantity
-        )
-
-    else:
-        instance.planned_quantity = instance.open_quantity
-        if instance.delivered_quantity != 0:
-            instance.open_quantity = (
-                instance.planned_quantity - instance.delivered_quantity
-            )
-
-
 def create_unload(sender, instance, created, **kwargs):
+    print("called")
     if instance.order_status == "Pending":
         purchase_order = instance
         status = "Not Started"
-        site_id = instance.site_id
-        target_area = instance.site_id.areas.get(area_name="REC_BAY")
-        Unload.objects.create(
+        site_id = instance.facility.site
+        areas = instance.facility.site.areas.all()
+        target_area = areas.filter(area_name__icontains="REC_BAY")[0]
+        unload = Unload.objects.create(
             status=status,
             site_id=site_id,
-            target_logistic_area=target_area,
+            logistic_area=target_area,
             purchase_order=purchase_order,
         )
 
-
-def create_unload_product(sender, instance, created, **kwargs):
-    print(instance.purchase_order.order_status)
-    if instance.purchase_order.order_status == "Pending":
-        PurchaseOrderTransaction.objects.create(
-            product=instance,
-            purchase_order=instance.purchase_order,
-            delivered_quantity=instance.delivered_quantity,
+        objs = map(
+            lambda product: PurchaseOrderTransaction(
+                product=product,
+                purchase_order=instance,
+                delivered_quantity=product.delivered_quantity,
+            ),
+            instance.po_products.all(),
         )
-        transactions = PurchaseOrderTransaction.objects.filter(
-            product=instance, purchase_order=instance.purchase_order
-        ).order_by("-id")
+        PurchaseOrderTransaction.objects.bulk_create(list(objs))
 
-        try:
-            open_quantity = (
-                transactions[0].delivered_quantity
-                - transactions[1].delivered_quantity
+        objs = []
+        for product in instance.po_products.all():
+            transactions = PurchaseOrderTransaction.objects.filter(
+                product=product,
+                purchase_order=instance,
+            ).order_by("-id")
+
+            try:
+                open_quantity = (
+                    transactions[0].delivered_quantity
+                    - transactions[1].delivered_quantity
+                )
+            except Exception:
+                open_quantity = transactions[0].delivered_quantity
+
+            unload_product = UnloadProduct(
+                unload=unload,
+                product=product.product,
+                open_quantity=open_quantity,
+                planned_quantity=open_quantity,
             )
-        except Exception:
-            open_quantity = transactions[0].delivered_quantity
+            objs.append(unload_product)
 
-        unload = instance.purchase_order.unload_set.all().last()
-        UnloadProduct.objects.create(
-            unload=unload,
-            purchase_product=instance,
-            open_quantity=open_quantity,
-        )
+        UnloadProduct.objects.bulk_create(objs)
 
 
-def create_putaway(sender, instance, created, **kwargs):
-
-    if not created:
-        if instance.status == "Finished":
-            status = "Not Started"
-            site_id = instance.site_id
-            final_unload = instance
-            source_area = instance.target_logistic_area
-
-            putaway = PutAway.objects.create(
-                status=status,
-                site_id=site_id,
-                final_unload=final_unload,
-                source_logistic_area=source_area,
-            )
-
-            for product in instance.unload_products.all():
-                objs = []
-                for stk in product.identified_stock.all():
-                    stock_identifier = stk.stock_identifier
-                    open_quantity = stk.split_quantity
-                    batch = stk.batch_number
-                    expiration_date = stk.expiration_date
-
-                    objs.append(
-                        PutAwayProduct(
-                            putaway=putaway,
-                            unload_product=product,
-                            target_logistic_area=check_area(
-                                product.purchase_product.product
-                            ),
-                            open_quantity=open_quantity,
-                            batch=batch,
-                            expiration_date=expiration_date,
-                            stock_identifier=stock_identifier,
-                        )
-                    )
-
-                PutAwayProduct.objects.bulk_create(objs)
-            print("PutAway Created !!")
-
-
-pre_save.connect(update_open_quantity, PurchaseOrderProduct)
-post_save.connect(create_putaway, Unload)
 post_save.connect(create_unload, PurchaseOrder)
-post_save.connect(create_unload_product, PurchaseOrderProduct)

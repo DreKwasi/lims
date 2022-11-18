6f686385916f3c0dbdf2647c2388f209b95e236f
DreKwasi <andrewsboateng137@gmail.com>
2022-11-18 06:32:55 +0000

    Url Endpoints for APIs

--- lims_project/urls.py
+++ lims_project/urls.py
@@ -6,14 +6,21 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/formulary/", include("formulary.api.urls")),
+    path("formulary/api/", include("formulary.api.urls")),
+    path("formulary/", include("formulary.urls")),
+    path("inventory/api/", include("inventory.api.urls")),
+    path("inventory/", include("inventory.urls")),
+    path("accounts/api/", include("accounts.api.urls")),
+    path("acounts/", include("accounts.urls")),
+    path("inbound/api/", include("inbound.api.urls")),
+    path("inbound/", include("inbound.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 urlpatterns += [
     path(
         "",
-        TemplateView.as_view(template_name="index.html"),
+        TemplateView.as_view(template_name="base.html"),
         name="home",
     )
 ]

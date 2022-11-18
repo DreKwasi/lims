e99735704221cc894c4082f1cf5d154abe75e352
DreKwasi <andrewsboateng137@gmail.com>
2022-11-18 06:04:49 +0000

    Model Manager for CustomQueryset for Purchase Order Model and Unload model

--- inbound/model_managers.py
+++ inbound/model_managers.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.dispatch import Signal
 
 from .queryset import CustomQuerySet
+
+post_save = Signal()
 
 
 class PurchaseOrderManager(models.Manager):
@@ -8,16 +11,11 @@ class PurchaseOrderManager(models.Manager):
         return CustomQuerySet(self.model, using=self._db)
 
 
-class PurchaseOrderProductManager(models.Model):
+class PurchaseOrderProductManager(models.Manager):
     def get_queryset(self):
         return CustomQuerySet(self.model, using=self._db)
 
 
 class UnloadManager(models.Manager):
-    def get_queryset(self):
-        return CustomQuerySet(self.model, using=self._db)
-
-
-class PutAwayManager(models.Manager):
     def get_queryset(self):
         return CustomQuerySet(self.model, using=self._db)

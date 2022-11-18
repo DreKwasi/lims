6cc2ab13885e904ae26fad73485cc6b0f53f375a
DreKwasi <andrewsboateng137@gmail.com>
2022-11-18 06:30:12 +0000

    Routes for Page Urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,27 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
-    path("", views.inventory, name="inventory"),
-    path("warehouse", views.warehouse, name="warehouse"),
-    path("stock_transfer", views.stock_transfer, name="stock_transfer"),
-    path("add_warehouse", views.add_warehouse, name="add_warehouse"),
+    path(
+        "",
+        TemplateView.as_view(template_name="inventory/inventory.html"),
+        name="inventory",
+    ),
+    path(
+        "stock_transfer",
+        TemplateView.as_view(template_name="inventory/stock_transfer.html"),
+        name="stock_transfer",
+    ),
+    path(
+        "warehouse",
+        TemplateView.as_view(template_name="inventory/add_warehouse.html"),
+        name="warehouse",
+    ),
+    path(
+        "add_warehouse",
+        TemplateView.as_view(template_name="inventory/add_warehouse.html"),
+        name="add_warehouse",
+    ),
 ]

ab81a37488374512830e3bb4f631057e8e28db13
DreKwasi <andrewsboateng137@gmail.com>
2022-11-18 06:29:19 +0000

    Addons for Inventory Models and Site

--- inventory/models.py
+++ inventory/models.py
@@ -1,19 +1,24 @@
 import uuid
 
 from accounts.models import Facility
+from django.core.validators import MinValueValidator
 from django.db import models
 from formulary.models import ProductList
 
 
 class Site(models.Model):
     facility = models.OneToOneField(
-        "accounts.facility", on_delete=models.CASCADE
+        "accounts.facility", on_delete=models.CASCADE, related_name="site"
     )
     created_date = models.DateTimeField(auto_now_add=True)
 
     @property
     def site_id(self):
         return "SITE_" + self.facility.facility_name.lower().replace(" ", "_")
+
+    @property
+    def facility_name(self):
+        return self.facility.facility_name
 
     def __str__(self):
         return self.site_id
@@ -30,6 +35,15 @@ class LogisticArea(models.Model):
         return self.area_name
 
 
+class InventoryManager(models.Model):
+    stock_type_choices = (
+        ("Warehouse", "Warehouse"),
+        ("Pre-Delivered", "Pre-Delivered"),
+        ("In-Transit", "In-Transit"),
+    )
+    stock_type = models.CharField(max_length=100, choices=stock_type_choices)
+
+
 class Inventory(models.Model):
 
     stock_type_choices = (
@@ -37,15 +51,19 @@ class Inventory(models.Model):
         ("Pre-Delivered", "Pre-Delivered"),
         ("In-Transit", "In-Transit"),
     )
-
     stock_type = models.CharField(max_length=100, choices=stock_type_choices)
     product = models.ForeignKey(ProductList, on_delete=models.CASCADE)
     batch = models.CharField(max_length=225, blank=True, null=True)
-    pack_quantity = models.IntegerField(verbose_name="Number of packs")
-    logistic_area = models.ForeignKey(
-        "inventory.logisticarea", on_delete=models.SET_NULL, null=True
+    pack_quantity = models.PositiveIntegerField(
+        verbose_name="Number of packs", validators=[MinValueValidator(1)]
     )
-    stock_identifier = models.UUIDField(default=uuid.uuid4(), blank=True)
+    logistic_area = models.ForeignKey(
+        "inventory.logisticarea",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="inventory_areas",
+    )
+    stock_identifier = models.UUIDField(default=uuid.uuid4, blank=True)
     expiration_date = models.DateTimeField()
     created_date = models.DateField(auto_now_add=True)
 
@@ -64,5 +82,3 @@ class Inventory(models.Model):
     def site_id(self):
         return self.logistic_area.site_id
 
-
-# signals to check logistic area and siteid

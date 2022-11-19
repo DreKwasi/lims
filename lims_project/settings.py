ac05c3d33ec8f7f74d8a77a6041ff04d0f808c1e
DreKwasi <andrewsboateng137@gmail.com>
2022-11-19 17:25:13 +0000

    Migrating from SQLite to Postgres

--- lims_project/settings.py
+++ lims_project/settings.py
@@ -92,10 +92,21 @@ AUTH_USER_MODEL = "accounts.Account"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "lims_db",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 

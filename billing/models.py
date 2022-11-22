dce01c6784b56c1d6c2e2224409dc0d9f226df6c
DreKwasi <andrewsboateng137@gmail.com>
2022-11-22 00:50:15 +0000

    Added Created Date and Updated Date

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class PriceList(models.Model):
+    product = models.ForeignKey(
+        "formulary.productlist",
+        on_delete=models.CASCADE,
+    )
+    cost_price = models.FloatField()
+    selling_price = models.FloatField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)

b97150ebad1ec078f87db2790e342c1ecc601dd0
DreKwasi <andrewsboateng137@gmail.com>
2022-11-24 03:11:35 +0000

    Pick and Pack Model Created

--- outbound/models.py
+++ outbound/models.py
@@ -1,4 +1,9 @@
+import uuid
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from inventory.models import Inventory
 
 # Create your models here.
 
@@ -10,18 +15,22 @@ class StockTransfer(models.Model):
         ("Partial", "Partial"),
         ("Finished", "Finished"),
         ("Delivered", "Delivered"),
+        ("Not Started", "Not Started"),
     )
     ship_to_location = models.ForeignKey(
         "accounts.facility",
-        related_name="facility_sent_transfers",
+        related_name="facility_transfers",
         on_delete=models.SET_NULL,
         null=True,
     )
     ship_from_location = models.ForeignKey(
         "accounts.facility",
-        related_name="facility_receive_transfers",
+        related_name="facility_receipts",
         on_delete=models.SET_NULL,
         null=True,
+    )
+    status = models.CharField(
+        max_length=20, choices=status_choices, default="Not Started"
     )
     order_priority = models.CharField(
         choices=order_priority_choices, default="Normal", max_length=10
@@ -39,13 +48,12 @@ class StockTransfer(models.Model):
         return f"ST-{self.id}"
 
 
-class StockTransferProducts(models.Model):
+class StockTransferProduct(models.Model):
     uom_choices = (
-        ("pack", "pack(s)"),
-        ("batch", "batch"),
-        ("volume", "volume"),
-        ("weight", "weight"),
-        ("unit", "unit"),
+        ("Packs", "Packs"),
+        ("Units", "Units"),
+        ("Volume in Mls", "Volume in Mls"),
+        ("Weight in Grams", "Weight in Grams"),
     )
     status_choices = (
         ("Not Started", "Not Started"),
@@ -61,18 +69,104 @@ class StockTransferProducts(models.Model):
         on_delete=models.CASCADE,
         null=True,
     )
-    products = models.ForeignKey(
+    product = models.ForeignKey(
         "formulary.productlist",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
         related_name="formulary_stock_transfer",
     )
-    inventory_level = models.IntegerField()
     quantity = models.PositiveIntegerField(default=1)
-    unit_of_measure = models.CharField(max_length=20, choices=uom_choices)
+    unit_of_measure = models.CharField(
+        max_length=20, choices=uom_choices, default="Packs"
+    )
     status = models.CharField(
         max_length=20, default="Not Started", choices=status_choices
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def inventory_level(self):
+        batch_qtys = Inventory.objects.filter(
+            product=self.product
+        ).values_list("pack_quantity", flat=True)
+        return sum(batch_qtys)
+
+
+class PickPack(models.Model):
+    order_priority_choices = (("Normal", "Normal"), ("Urgent", "Urgent"))
+    status_choices = (
+        ("Cancelled", "Cancelled"),
+        ("Finished", "Finished"),
+        ("Not Started", "Not Started"),
+    )
+    ship_to_location = models.ForeignKey(
+        "accounts.facility",
+        related_name="facility_packs",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    ship_from_location = models.ForeignKey(
+        "accounts.facility",
+        related_name="facility_picks",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    status = models.CharField(
+        max_length=20, choices=status_choices, default="Not Started"
+    )
+    order_priority = models.CharField(
+        choices=order_priority_choices, default="Normal", max_length=10
+    )
+    user = models.ForeignKey(
+        "accounts.account",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="user_pickpack",
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"PP-{self.id}"
+
+
+class PickPackProduct(models.Model):
+    uom_choices = (
+        ("Packs", "Packs"),
+        ("Units", "Units"),
+        ("Volume in Mls", "Volume in Mls"),
+        ("Weight in Grams", "Weight in Grams"),
+    )
+    pickpack = models.ForeignKey(
+        PickPack,
+        related_name="pickpack_products",
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    product = models.ForeignKey(
+        "formulary.productlist",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="formulary_pickpack",
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    logistic_area = models.ForeignKey(
+        "inventory.logisticarea", on_delete=models.SET_NULL, null=True
+    )
+    batch_number = models.CharField(max_length=225, null=True, blank=True)
+    expiration_date = models.DateField()
+    stock_identifier = models.UUIDField(
+        verbose_name=_("Unique Identifier"),
+        editable=False,
+        null=True,
+        blank=True,
+        unique=True,
+    )
+    unit_of_measure = models.CharField(
+        max_length=20, choices=uom_choices, default="Packs"
     )
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
@@ -93,23 +187,32 @@ class SalesOrder(models.Model):
         on_delete=models.SET_NULL,
         null=True,
     )
+    user = models.ForeignKey(
+        "accounts.account",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="user_sales",
+    )
     order_priority = models.CharField(
         choices=order_priority_choices, default="Normal", max_length=20
     )
     status = models.CharField(
         choices=status_choices, default="Not Started", max_length=20
     )
+    external_reference = models.CharField(max_length=20, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"SO-{self.id}"
 
-class SalesOrdersProducts(models.Model):
+
+class SalesOrderProduct(models.Model):
     uom_choices = (
-        ("pack", "pack"),
-        ("batch", "batch"),
-        ("volume", "volume"),
-        ("kg", "kg"),
-        ("unit", "unit"),
+        ("Packs", "Packs"),
+        ("Units", "Units"),
+        ("Volume in Mls", "Volume in Mls"),
+        ("Weight in Grams", "Weight in Grams"),
     )
     status_choices = (
         ("Not Started", "Not Started"),
@@ -119,13 +222,13 @@ class SalesOrdersProducts(models.Model):
         ("Released", "Released"),
         ("Delivered", "Delivered"),
     )
-    stock_transfer_order = models.ForeignKey(
-        StockTransfer,
+    sales_order = models.ForeignKey(
+        SalesOrder,
         related_name="sales_order_products",
         on_delete=models.CASCADE,
         null=True,
     )
-    products = models.ForeignKey(
+    product = models.ForeignKey(
         "formulary.productlist",
         on_delete=models.CASCADE,
         null=True,
@@ -137,6 +240,12 @@ class SalesOrdersProducts(models.Model):
     )
     inventory_level = models.IntegerField()
     quantity = models.PositiveIntegerField(default=1)
+    ship_from_location = models.ForeignKey(
+        "accounts.facility",
+        related_name="facility_sales_transfers",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     unit_of_measure = models.CharField(
         max_length=20, choices=uom_choices, default="pack"
     )

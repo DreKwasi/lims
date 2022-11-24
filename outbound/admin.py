b2ef28a8ec287db036f30bbcc18d11b0dc0cd2ff
DreKwasi <andrewsboateng137@gmail.com>
2022-11-24 03:11:13 +0000

    Admin for Stock Transfers and Sales Orders

--- outbound/admin.py
+++ outbound/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class StockTransferAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "ship_to_location",
+        "ship_from_location",
+        "order_priority",
+        "status",
+        "created_date",
+        "updated_date",
+    ]
+
+
+admin.site.register(StockTransfer, StockTransferAdmin)
+
+
+class StockTransferProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "stock_transfer_order",
+        "product",
+        "inventory_level",
+        "quantity",
+        "unit_of_measure",
+        "status",
+        "created_date",
+        "updated_date",
+    ]
+
+
+admin.site.register(StockTransferProduct, StockTransferProductAdmin)
+
+
+class SalesOrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "account",
+        "order_priority",
+        "status",
+        "created_date",
+        "updated_date",
+    ]
+
+
+admin.site.register(SalesOrder, SalesOrderAdmin)
+
+
+class SalesOrderProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "sales_order",
+        "product",
+        "inventory_level",
+        "quantity",
+        "unit_of_measure",
+        "status",
+    ]
+
+
+admin.site.register(SalesOrderProduct, SalesOrderProductAdmin)

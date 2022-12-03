1f28e4940e26b879dfccbe4dcfc3abdfc99ffaec
DreKwasi <andrewsboateng137@gmail.com>
2022-12-03 21:55:20 +0000

    Model managers for included CustomQueryset

--- outbound/model_managers.py
+++ outbound/model_managers.py
@@ -5,3 +5,13 @@ from utils.queryset import CustomQuerySet
 class StockTransferManager(models.Manager):
     def get_queryset(self):
         return CustomQuerySet(self.model, using=self._db)
+
+
+class PickPackManager(models.Manager):
+    def get_queryset(self):
+        return CustomQuerySet(self.model, using=self._db)
+
+
+class DeliveryManager(models.Manager):
+    def get_queryset(self):
+        return CustomQuerySet(self.model, using=self._db)

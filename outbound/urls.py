31c56489b791dce6cbe143058b04ee386b0afa81
DreKwasi <andrewsboateng137@gmail.com>
2022-12-14 23:58:50 +0000

    Url patterns for stock transfers

--- outbound/urls.py
+++ outbound/urls.py
@@ -1,3 +1,22 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "",
+        TemplateView.as_view(template_name="outbound/stock_transfer.html"),
+        name="stock_transfers",
+    ),
+    path(
+        "new_stock_transfer",
+        TemplateView.as_view(template_name="outbound/new_stock_transfer.html"),
+        name="new_stock_transfer",
+    ),
+    path(
+        "edit_stock_transfer",
+        TemplateView.as_view(
+            template_name="outbound/edit_stock_transfer.html"
+        ),
+        name="edit_stock_transfer",
+    ),
+]

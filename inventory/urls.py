2f46fd4f5e7bb2c1496030baf286b427c61a38c4
DreKwasi <andrewsboateng137@gmail.com>
2022-12-15 00:31:07 +0000

    Removed Stock Transfers from Routing

--- inventory/urls.py
+++ inventory/urls.py
@@ -10,17 +10,12 @@ urlpatterns = [
         name="inventory",
     ),
     path(
-        "stock_transfer",
-        TemplateView.as_view(template_name="inventory/stock_transfer.html"),
-        name="stock_transfer",
-    ),
-    path(
-        "warehouse",
+        "warehouse/",
         TemplateView.as_view(template_name="inventory/add_warehouse.html"),
         name="warehouse",
     ),
     path(
-        "add_warehouse",
+        "add_warehouse/",
         TemplateView.as_view(template_name="inventory/add_warehouse.html"),
         name="add_warehouse",
     ),

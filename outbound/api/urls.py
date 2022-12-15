7ca3721ae8d15dec0ef9b8070386aeb613baf0f0
DreKwasi <andrewsboateng137@gmail.com>
2022-12-15 00:58:06 +0000

    Url routing for Stock Transfers and Products

--- outbound/api/urls.py
+++ outbound/api/urls.py
@@ -1,3 +1,13 @@
-from django.urls import include, path
+from django.urls import include, path #modules for managing django url routing
+from rest_framework.routers import DefaultRouter  # handling api routes
+from .views import StockTransferApiViewSet  # import needed viewset for routing
 
-urlpatterns = []
+router = DefaultRouter() #Initialize DefaultRouter object to a variable
+
+# Register required API Routes to Viewsets
+router.register(
+    r"stock_transfers", StockTransferApiViewSet, basename="stock_transfers"
+)
+
+# Include API routes to DjangoUrl Routing
+urlpatterns = [path("", include(router.urls))]

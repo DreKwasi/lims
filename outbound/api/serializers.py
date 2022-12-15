4d23baf4317c95226e3fe1fbc5b48aeaf2887f56
DreKwasi <andrewsboateng137@gmail.com>
2022-12-15 19:22:34 +0000

    Stock Transfer and products Serializer merge

--- outbound/api/serializers.py
+++ outbound/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from accounts.models import Account, Facility
 from formulary.models import ProductList
 
 from ..models import StockTransfer, StockTransferProduct
@@ -16,6 +17,22 @@ class StockTransferProductSerializer(serializers.Serializer):
         queryset=ProductList.objects.all(),
         required=False,
     )
+    status = serializers.ChoiceField(
+        choices=[
+            "Released",
+            "Not Started" "Cancelled",
+            "Not Released",
+            "Partial",
+            "Delivered",
+        ]
+    )
+    unit_of_measure = serializers.ChoiceField(
+        choices=["Packs", "Units", "Volume in Mls", "Weight in Grams"]
+    )
+    quantity = serializers.IntegerField()
+    inventory_level = serializers.IntegerField()
+    created_date = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    updated_date = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
 
     class Meta:
         model = StockTransferProduct
@@ -32,12 +49,27 @@ class StockTransferProductSerializer(serializers.Serializer):
 
 
 class StockTransferSerializer(serializers.ModelSerializer):
-    stock_transfer_products = StockTransferProductSerializer(many=True)
+    stock_transfer_products = StockTransferProductSerializer(
+        many=True, read_only=True
+    )
+    ship_to_location = serializers.SlugRelatedField(
+        queryset=Facility.objects.all(), slug_field="facility_name"
+    )
+    ship_from_location = serializers.SlugRelatedField(
+        queryset=Facility.objects.all(), slug_field="facility_name"
+    )
+    user = serializers.SlugRelatedField(
+        slug_field="username", queryset=Account.objects.all()
+    )
+    created_date = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    updated_date = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
 
     class Meta:
         model = StockTransfer
         fields = [
-            "__str__",
+            "id",
+            "stock_transfer_id",
+            "stock_transfer_products",
             "ship_to_location",
             "ship_from_location",
             "status",
